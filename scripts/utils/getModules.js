a53626c6330ba1975bf3b417ef5a3a2b36f21d47
Lewis Moten <lewis.moten@codejamboree.com>
2024-09-01 18:26:51 -0400

    add file name pattern detection for tests

--- scripts/utils/getModules.js
+++ scripts/utils/getModules.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-export const getModules = async (dir) => {
+export const getModules = async (dir, pattern = /\.test\.js$/, replacement = '') => {
   if (!fs.existsSync(dir)) return;
   const files = fs.readdirSync(dir);
   if (files.length === 0) return;
@@ -11,14 +11,16 @@ export const getModules = async (dir) => {
   for (let i = 0; i < files.length; i++) {
     const file = files[i];
     const filePath = path.join(dir, file);
-    const name = path.basename(file).replace(path.extname(file), '');
     if (fs.lstatSync(filePath).isDirectory()) {
       const children = await getModules(filePath);
       if (children) {
-        modules[name] = children;
+        modules[file] = children;
       }
     } else {
-      modules[name] = await import('../../' + filePath);
+      if (pattern.test(file)) {
+        const name = file.replace(pattern, replacement);
+        modules[name] = await import('../../' + filePath);
+      }
     }
   }
   if (Object.keys(modules) === 0) return;
