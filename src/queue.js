2c52348bc81d2bc9c505f649aa42e2476a599942
Lewis Moten <lewis.moten@codejamboree.com>
2024-09-02 15:27:38 -0400

    return promise if callbacks missing

--- src/queue.js
+++ src/queue.js
@@ -6,25 +6,30 @@ export const queue = (...args) => {
     throw new Error(`Not allowing new requests.`);
   }
 
-  const params = parseParams(args);
+  const { promise, ...params } = parseParams(args);
 
   checkArgs(params.args);
 
   state.append('queue', params);
   startTimers();
+
+  return promise;
 }
 
 const parseParams = (args) => {
   const [first] = args;
+
   switch (typeof first) {
     case 'string':
-      return { args };
+      return argsAsHttpsArgs(args);
     case 'object':
-      if (first instanceof URL) return { args };
-      if (!('args' in first)) {
-        throw new Error(`Missing args key`);
+      if (first instanceof URL) {
+        return argsAsHttpsArgs(args);
       }
-      return first;
+      if (args.length === 1 && 'args' in first) {
+        return argsAsQueueParam(first);
+      }
+      return argsAsHttpsArgs(args);
     default:
       throw new Error(`Unexpected arguments. Need args for https.request, or object as first arg`);
   }
@@ -35,4 +40,39 @@ const checkArgs = args => {
   if (args.length > 0) return;
   const unexpected = Array.isArray(args) ? typeof args : args.length;
   throw new Error(`Expected one or more requestArguments. Got ${unexpected}.`);
+}
+
+const argsAsHttpsArgs = (args) => {
+  let onRequested, onCancel;
+  const promise = new Promise((resolve, reject) => {
+    onRequested = resolve;
+    onCancel = reject;
+  });
+  return {
+    args,
+    promise,
+    onRequested,
+    onCancel
+  };
+}
+const argsAsQueueParam = (params) => {
+  const { args, onRequested, onCancel, ...rest } = params;
+  const keys = Object.keys(rest);
+  keys.forEach(key => {
+    console.debug(`Unrecognized key: ${key}`);
+  });
+  if (!['function', 'undefined'].includes(typeof onRequested)) {
+    throw new Error(`Expected onRequested to be function (${typeof onRequested})`);
+  }
+  if (!['function', 'undefined'].includes(typeof onCancel)) {
+    throw new Error(`Expected onCancel to be function (${typeof onCancel})`);
+  }
+  if (!Array.isArray(args)) {
+    throw new Error(`Expected args to be array. (${typeof args})`);
+  }
+  if (args.length === 0) {
+    throw new Error(`Expected one or more args. (0)`);
+  }
+
+  return params;
 }
