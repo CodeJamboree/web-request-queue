2b1b6c825667526d99ec877ab4758a217adb7c34
Lewis Moten <lewis.moten@codejamboree.com>
2024-09-01 22:32:48 -0400

    rename tests

--- src/request/msPerRequest.test.js
+++ src/request/msPerRequest.test.js
@@ -2,19 +2,19 @@ import { expect } from '../../scripts/utils/expect.js';
 import { state } from '../state.js';
 import { msPerRequest } from './msPerRequest.js';
 
-export const msPerRequest10perSecond = () => {
+export const tenPerSecond = () => {
   state.set('throttleCount', 10);
   state.set('throttleSeconds', 1);
   expect(msPerRequest()).is(100)
 }
 
-export const msPerRequest1kPerSecond = () => {
+export const oneThousandPerSecond = () => {
   state.set('throttleCount', 1000);
   state.set('throttleSeconds', 1);
   expect(msPerRequest()).is(1)
 }
 
-export const msPerRequest1kPerMinute = () => {
+export const oneThousandPerMinute = () => {
   state.set('throttleCount', 1000);
   state.set('throttleSeconds', 60);
   expect(msPerRequest()).is(60)
