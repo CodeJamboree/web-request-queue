239edec8e82d6cbf60444212c65fa135a8e2193e
Lewis Moten <lewis.moten@codejamboree.com>
2024-09-09 21:19:06 -0400

    auto-close stream and return type

--- src/toStream.ts
+++ src/toStream.ts
@@ -3,13 +3,13 @@ import { requestArgs } from "./global.js";
 import { queue } from './queue.js';
 import { handleEnd, handleError, handleResponse } from "./utils/handlers.js";
 
-export const toStream = async (stream: Writable, ...args: requestArgs): Promise<Writable> => {
-  return new Promise<Writable>(async (resolve, reject) => {
+export const toStream = async <T extends Writable>(stream: T, ...args: requestArgs): Promise<T> => {
+  return new Promise<T>(async (resolve, reject) => {
     handleError(stream, reject);
     handleEnd(stream, () => resolve(stream));
     const req = await queue(...args);
     handleResponse(req, res => {
-      res.pipe(stream);
+      res.pipe(stream, { end: true });
       handleError(res, reject);
       handleEnd(res, () => resolve(stream));
     });
